398cae69814a6f036cce6edbcbae847a795b3a35
Brian Jing <brian.jing@myob.com>
2016-09-23 16:31:14 +1000

    Add missing file from last commit

--- test/Datastructures/ListSpec.hs
+++ test/Datastructures/ListSpec.hs
@@ -12,3 +12,9 @@ spec = do
   describe "Tail" $ do
     it "should return tail" $ do
       tail1 [1..5] `shouldBe` [2..5]
+    it "should return tail of an empty list" $ do
+      tail1 []  `shouldBe` ([] :: [Int])
+
+  describe "setHead" $ do
+    it "should set new head for list" $ do
+      setHead [1..3] 4 `shouldBe` [4, 1, 2, 3]
